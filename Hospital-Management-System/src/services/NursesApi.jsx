de4be257e698f9ea04138962c7161d7041d26b4c
andrewhany123 <36668421+andrewhany123@users.noreply.github.com>
2019-05-09 16:32:50 +0200

    Add files via upload

--- Hospital-Management-System/src/services/NursesApi.jsx
+++ Hospital-Management-System/src/services/NursesApi.jsx
@@ -1,48 +1,39 @@
-import Axios from 'axios';
+import Axios from "axios";
 
 class NursesApi {
+  static getNurses = callback => {
+    Axios.get("api/nurses")
+      .then(res => callback(res.data))
+      .catch(NursesApi.errorHandler);
+  };
 
+  static getNurse = (id, callback) => {
+    Axios.get("api/nurses/" + id)
+      .then(res => callback(res.data))
+      .catch(NursesApi.errorHandler);
+  };
 
-    static getNurses = (callback) => {
-        Axios.get('api/nurses')
-            .then(res => callback(res.data))
-            .catch(NursesApi.errorHandler);
-    }
+  static addNurse = (patient, callback) => {
+    Axios.post("api/nurses", patient)
+      .then(() => NursesApi.getNurses(callback))
+      .catch(NursesApi.errorHandler);
+  };
 
+  static editNurse = (patient, callback) => {
+    let id = patient.id;
+    delete patient.id;
+    Axios.put("api/nurses/" + id, patient)
+      .then(() => NursesApi.getNurses(callback))
+      .catch(NursesApi.errorHandler);
+  };
 
-    static getNurse = (id, callback) => {
-        Axios.get('api/nurses/' + id)
-            .then(res => callback(res.data))
-            .catch(NursesApi.errorHandler);
-    }
+  static deleteNurse = (id, callback) => {
+    Axios.delete("api/nurses/" + id)
+      .then(() => NursesApi.getNurses(callback))
+      .catch(NursesApi.errorHandler);
+  };
 
-
-    static addNurse = (patient, callback) => {
-        Axios.post('api/nurses', patient)
-            .then(() => NursesApi.getPatients(callback))
-            .catch(NursesApi.errorHandler);
-    }
-
-
-    static editNurse = (patient, callback) => {
-        let id = patient.id;
-        delete patient.id;
-        Axios.put('api/nurses/' + id, patient)
-            .then(() => NursesApi.getPatients(callback))
-            .catch(NursesApi.errorHandler);
-    }
-
-
-    static deleteNurse = (id, callback) => {
-        Axios.delete('api/nurses/' + id)
-            .then(() => NursesApi.getPatients(callback))
-            .catch(NursesApi.errorHandler);
-    }
-
-
-    errorHandler = error => console.log(error);
-
+  errorHandler = error => console.log(error);
 }
-
 
 export default NursesApi;
